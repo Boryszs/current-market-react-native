cd0c896be128689739d1d44fc7e27345fd4feaff
Boryszs <12damie@gmail.com>
2021-03-06 23:44:46 +0100

    divide screen and navigation to other file

---
+++ screens/CurrencyScrenn.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Button, View ,Text} from 'react-native';
+
+export default CurrencyScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>sadsad</Text>
+    </View>
+  );
+}
---
+++ screens/ExchangeScreen.js
@@ -0,0 +1,11 @@
+const { exp } = require("react-native-reanimated");
+import * as React from 'react';
+import { Button, View ,Text} from 'react-native';
+
+export default ExchangeScreen = ({ navigation }) => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>sadsad</Text>
+      </View>
+    );
+  }

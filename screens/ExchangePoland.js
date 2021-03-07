b6223a80b5a67f804756dd76cf3de1af50ceaf45
Boryszs <12damie@gmail.com>
2021-03-07 20:33:18 +0100

    implement exchange refactor code

---
+++ screens/ExchangePoland.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Button, View, Text, FlatList, SafeAreaView, ScrollView } from 'react-native';
+
+export default class CurrencyScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            plExchange: []
+        }
+    }
+    async componentDidMount() {
+        try {
+            await this.getPlExchamge();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async getPlExchamge() {
+        await fetch('http://192.168.56.1:8080/exchange/pl/all', {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    plExchange: responseJson
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <FlatList
+                    data={this.state.plExchange}
+                    keyExtractor={item => item.name}
+                    ListHeaderComponent={() => {
+                        return (
+                            <View style={{ flex: 1, flexDirection: 'row', width: '100%', height: '100%', marginLeft: 10, marginBottom: 10 }}>
+                                <Text style={{ width: '20%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Name.'}</Text>
+                                <Text>{" "}</Text>
+                                <Text style={{ width: '48%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Course.'}</Text>
+                                <Text>{" "}</Text>
+                                <Text style={{ width: '30%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Change.'}</Text>
+                            </View>)
+                    }}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <View key={index} style={{ flex: 1, flexDirection: 'row', width: '100%', height: '100%', marginLeft: 10, marginBottom: 15 }}>
+                                <Text style={{ width: '40%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.name}</Text>
+                                <Text>{" "}</Text>
+                                <Text style={{ width: '30%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.course}</Text>
+                                <Text>{" "}</Text>
+                                <Text style={{ width: '30%', fontSize: 18, textAlign: 'center', fontSize: 16 }}>{item.change}</Text>
+                            </View>
+                        )
+                    }}
+                />
+            </View>
+        )
+    }
+
+}
--- screens/ExchangeWorld.js
+++ screens/ExchangeWorld.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import { Button, View, Text, FlatList, SafeAreaView, ScrollView } from 'react-native';
 
-export default class CryptoCurrencyScreen extends React.Component {
+export default class ExchangeWorld extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      crypto: []
+      wExchange: []
     }
   }
-
   async componentDidMount() {
     try {
-      await this.getCryptoCurrency();
+      await this.getPlExchamge();
     } catch (err) {
       console.error(err);
     }
   }
 
-  async getCryptoCurrency() {
-    await fetch('http://192.168.56.1:8080/crypt-currency/important', {
+  async getPlExchamge() {
+    await fetch('http://192.168.56.1:8080/exchange/w/all', {
       method: 'GET'
     })
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
-          crypto: responseJson
+          wExchange: responseJson
         })
       })
       .catch((error) => {
@@ -37,14 +36,14 @@ export default class CryptoCurrencyScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <FlatList
-          data={this.state.crypto}
+          data={this.state.wExchange}
           keyExtractor={item => item.name}
           ListHeaderComponent={() => {
             return (
               <View style={{ flex: 1, flexDirection: 'row', width: '100%', height: '100%', marginLeft: 10, marginBottom: 10 }}>
                 <Text style={{ width: '20%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Name.'}</Text>
                 <Text>{" "}</Text>
-                <Text style={{ width: '48%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Average value.'}</Text>
+                <Text style={{ width: '48%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Course.'}</Text>
                 <Text>{" "}</Text>
                 <Text style={{ width: '30%', fontSize: 18, textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{'Change.'}</Text>
               </View>)
@@ -52,16 +51,17 @@ export default class CryptoCurrencyScreen extends React.Component {
           renderItem={({ item, index }) => {
             return (
               <View key={index} style={{ flex: 1, flexDirection: 'row', width: '100%', height: '100%', marginLeft: 10, marginBottom: 15 }}>
-                <Text style={{ width: '22%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.name}</Text>
+                <Text style={{ width: '40%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.name}</Text>
                 <Text>{" "}</Text>
-                <Text style={{ width: '54%', fontSize: 18, textAlign: 'center', fontSize: 16 }}>{item.courseAverage}</Text>
+                <Text style={{ width: '30%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.course}</Text>
                 <Text>{" "}</Text>
-                <Text style={{ width: '24%', fontSize: 18, textAlign: 'left', fontSize: 16 }}>{item.change}</Text>
+                <Text style={{ width: '30%', fontSize: 18, textAlign: 'center', fontSize: 16 }}>{item.change}</Text>
               </View>
             )
           }}
         />
       </View>
-    );
+    )
   }
+
 }
